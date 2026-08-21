dfd372da467fb403f52e146155a3b1d69816ac81
locTX-1002 <xuanloc10021506@gmail.com>
2026-08-21 08:35:51 +0700

    fix(leaderboard): import useMemo con thieu -> sua crash 'useMemo is not defined'
    LeaderboardView dung useMemo (topPodiumTeams) nhung import tu react chi co
    useState -> ReferenceError khi render trang /events/{id}/leaderboard.

--- src/views/LeaderboardView.tsx
+++ src/views/LeaderboardView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { Link } from "@/i18n/routing";
 import { ApiMissingDataBadge } from "@/components/ui";
 import { useEvents } from "@/repositories/eventsRepository";
